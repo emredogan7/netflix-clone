77ee659f8ea85adfd91e99e8a5de9a788878203d
Emre Doğan <emre.dogan@bilkent.edu.tr>
2021-04-24 19:05:11 +0300

    adding api key as an env variable

--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,14 @@
-const API_KEY = "fb6b17f00b4ac8272627e1caa7ec7593";
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}& =10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-  };
+  fetchTrending: `/trending/all/week?api_key=${TMDB_API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${TMDB_API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${TMDB_API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${TMDB_API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${TMDB_API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${TMDB_API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${TMDB_API_KEY}& =10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${TMDB_API_KEY}&with_genres=99`,
+};
 
-  export default requests;
+export default requests;
