d69381fae54d196a67aef7a4014e75b6428d58fd
Emre Doğan <emre.dogan@bilkent.edu.tr>
2021-04-24 13:17:22 +0300

    Added firebase and gh action

--- src/Banner.js
+++ src/Banner.js
@@ -42,7 +42,6 @@ function Banner() {
         <h1 className="banner__description">
           {truncate(movie?.overview, 250)}
         </h1>
-        {/* description */}
       </div>
       <div className="banner__fadeBottom"></div>
     </header>
--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const imgBaseUrl = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerURL, setTrailerURL] = useState("");
 
   // when this component loads, do some stuff, run sth.
   // for our case, fetch api!
@@ -23,6 +26,26 @@ function Row({ title, fetchUrl, isLargeRow }) {
   // everytime this changes, we need to update useEffect()
 
   // console.log(movies);
+  const youtubeOptions = {
+    height: "300",
+    width: "100%",
+  };
+
+  const handleClick = (movie) => {
+    console.warn(movie);
+    if (trailerURL) {
+      setTrailerURL("");
+    } else {
+      console.log(movie);
+      movieTrailer(movie?.original_title || movie?.name || "")
+        .then((url) => {
+          // console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerURL(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
   return (
     <div className="row">
       <h2> {title} </h2>
@@ -31,6 +54,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${imgBaseUrl}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -39,6 +63,9 @@ function Row({ title, fetchUrl, isLargeRow }) {
           ></img>
         ))}
       </div>
+      {/* <Youtube videoId={"ADQFNBZwEPo"} opts={youtubeOptions} /> */}
+      {trailerURL && <Youtube videoId={trailerURL} opts={youtubeOptions} />}
+
       {/* title */}
       {/* posters of movies */}
     </div>
